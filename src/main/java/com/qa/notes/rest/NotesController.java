f0c1299177eb5a790367ec8379f9890aba1584c9
Adama <akabba@qa.com>
2020-08-27 14:39:30 +0100

    initiate controller class

---
+++ src/main/java/com/qa/notes/rest/NotesController.java
@@ -0,0 +1,8 @@
+package com.qa.notes.rest;
+
+//controllers allow us to specify the urls against which the application will be responding
+//each url will connect tp a service which links to the repository built on to the entity class
+//better to have functionality split by pages
+
+public class NotesController {
+}
