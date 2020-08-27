627f7aa9b8c1c5f9154c10d8896f0a09e9479ef9
Adama <akabba@qa.com>
2020-08-27 14:53:02 +0100

    added all controller function addedd all notes to url

--- src/main/java/com/qa/notes/rest/NotesController.java
+++ src/main/java/com/qa/notes/rest/NotesController.java
@@ -4,5 +4,31 @@ package com.qa.notes.rest;
 //each url will connect tp a service which links to the repository built on to the entity class
 //better to have functionality split by pages
 
+import com.qa.notes.domain.Note;
+import com.qa.notes.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// RestController will convert data to a standard JSON
+@RestController
 public class NotesController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NotesController(NoteService noteService){
+        this.noteService = noteService;
+    }
+
+    //specifically used for read functionality/ browsers only able to work with getmapping
+    @GetMapping("/")
+    public List<Note> getAllNotes(){
+        return this.noteService.readAllNotes();
+    }
+
+
 }
